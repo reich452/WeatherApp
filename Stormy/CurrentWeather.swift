7dbf53268f502f0d2093de8fd7ac3e92c4b91e8c
reich452 <reich452@regis.edu>
2017-03-25 21:02:27 -0600

    Added WeatherIcon enum

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Nick Reichard on 3/25/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+
+struct CurrentWeather {
+    
+    let temperature: Double
+    let humidity: Double
+    let percipitationProbability: Double
+    let summary: String
+    let icon: String
+    
+}
