117438c98cd1d1c9a60e9961f4d9e3c7f2921698
reich452 <reich452@regis.edu>
2017-03-25 20:28:49 -0600

    Added Storyboard and outlets

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Pasan Premaratne on 2/15/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
